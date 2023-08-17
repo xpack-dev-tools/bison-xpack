212029828f5a6acf0314d40ed7b2d51719119a82
Liviu Ionescu <ilg@livius.net>
2023-08-17 12:01:49 +0300

    run.sh update

--- scripts/tests/run.sh
+++ scripts/tests/run.sh
@@ -14,7 +14,7 @@ function tests_run_all()
 {
   local test_bin_path="$1"
 
-  sed_test "${test_bin_path}"
+  bison_test "${test_bin_path}"
 }
 
 # -----------------------------------------------------------------------------
