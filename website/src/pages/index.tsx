f80157c48a79543c32e276f1946c81b930b95236
Liviu Ionescu <ilg@livius.net>
2024-10-10 21:37:00 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -41,9 +41,9 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from xPack Binary Development Tools`}
-      description="A binary package with the GNU bison executables">
+    <Layout 
+      title={`Hello from bison`}
+      description="A binary package with the bison executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
