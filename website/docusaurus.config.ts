89a386483d70d8e878f7337aa053b6e6432d576e
Liviu Ionescu <ilg@livius.net>
2025-01-10 13:20:38 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -89,7 +89,8 @@ console.log('customFields: ' + util.inspect(customFields));
 // ----------------------------------------------------------------------------
 
 const config: Config = {
-  title: 'xPack GNU bison',
+  title: 'xPack GNU bison' +
+    ((process.env.DOCUSAURUS_IS_PREVIEW === 'true') ? ' (preview)' : ''),
   tagline: 'A binary distribution of GNU bison',
   // Explicitly set in headTags.
   // favicon: '/img/favicon.ico',
@@ -260,7 +261,7 @@ const config: Config = {
       items: [
         {
           to: '/',
-          // label: 'Home',
+          label: 'bison',
           className: 'header-home-link',
           position: 'left'
         },
