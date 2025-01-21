026226bab8ff201fda646510c6d1ef1b41620598
Liviu Ionescu <ilg@livius.net>
2025-01-21 12:14:04 +0200

    website: re-generate commons

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -275,7 +275,7 @@ const config: Config = {
           label: 'GitHub',
           items: [
             {
-              label: `bison-xpack`,
+              label: `bison-xpack project`,
               href: `https://github.com/xpack-dev-tools/bison-xpack/`,
             },
             {
@@ -348,7 +348,7 @@ const config: Config = {
               href: 'https://www.paypal.com/donate/?hosted_button_id=5MFRG9ZRBETQ8',
             },
             {
-              label: 'GitHub bison-xpack',
+              label: 'GitHub bison-xpack project',
               href: 'https://github.com/xpack-dev-tools/bison-xpack/',
             },
             {
